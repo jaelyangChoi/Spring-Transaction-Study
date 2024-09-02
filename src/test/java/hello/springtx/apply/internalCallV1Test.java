944a0d8a3876c50bc0a8af92ff0f6285fa8564b5
Jealyang <jaelyangchoi@gmail.com>
2024-09-02 19:34:57 +0900

    트랜잭션 주의사항 - AOP의 한계

---
+++ src/test/java/hello/springtx/apply/internalCallV1Test.java
@@ -0,0 +1,63 @@
+package hello.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@SpringBootTest
+public class internalCallV1Test {
+
+    @Autowired
+    CallService service;
+
+    @Test
+    void printProxy() {
+        log.info("callService class={}", service.getClass());
+    }
+
+    @Test
+    void internalCall() {
+        service.internal();
+    }
+
+    @Test
+    void externalCall() {
+        service.external();
+    }
+
+
+    @TestConfiguration
+    static class Config {
+        @Bean
+        CallService callService() {
+            return new CallService();
+        }
+    }
+
+    @Slf4j
+    static class CallService {
+        public void external() {
+            log.info("call external");
+            printTxInfo();
+            internal(); //트랜잭션이 적용될까?
+        }
+
+        @Transactional
+        public void internal() {
+            log.info("call internal");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+}
