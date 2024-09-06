d6081a9d24cccaf816522f8946f7752d3711b840
Jealyang <jaelyangchoi@gmail.com>
2024-09-07 00:33:58 +0900

    외부 트랜잭션이 롤백되면 내부 트랜잭션도 롤백된다

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -97,4 +97,19 @@ public class BasicTxTest {
         txManager.commit(outer);
     }
 
+    @Test
+    @DisplayName("외부 트랜잭션이 롤백되면 내부 트랜잭션도 롤백된다")
+    void outer_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    }
+
 }
